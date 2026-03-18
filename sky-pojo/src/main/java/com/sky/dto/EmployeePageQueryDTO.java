2ed892a53748d1affd301a44c788e55684c80cc6
taiyidu <2464445898@qq.com>
2026-03-18 19:36:10 +0800

    完成员工分页查询的代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -1,19 +1,26 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiOperation("员工分页查询参数")
 public class EmployeePageQueryDTO implements Serializable {
 
     //员工姓名
+    @ApiModelProperty("员工姓名")
     private String name;
 
     //页码
+    @ApiModelProperty("页码")
     private int page;
 
     //每页显示记录数
+    @ApiModelProperty("每页显示记录数")
     private int pageSize;
 
 }
