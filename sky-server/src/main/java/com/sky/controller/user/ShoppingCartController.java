c16eec8f055a5bc21b5c83d6dce648b60654a4e8
taiyidu <2464445898@qq.com>
2026-04-04 20:38:30 +0800

    查看购物车业务功能开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,41 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@Api("c端-购物车接口")
+@RequestMapping("/user/shoppingCart")
+public class ShoppingCartController{
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @ApiOperation("添加购物车")
+    @PostMapping("/add")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("添加购物车商品信息：{}",shoppingCartDTO);
+        shoppingCartService.addShoppingCart(shoppingCartDTO);
+        return Result.success();
+    }
+    /**
+     * 查看购物车
+     */
+    @ApiOperation("查看购物车")
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        log.info("查看购物车");
+        List<ShoppingCart> list = shoppingCartService.list();
+        return Result.success(list);
+    }
+}
---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,33 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper{
+    /**
+     * 动态条件查询
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 根据id修改商品数量
+     * @param cart
+     */
+    @Update("update shopping_cart set number = #{number} where id= #{id}")
+    void updateNumberById(ShoppingCart cart);
+
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart (user_id, dish_id, setmeal_id, name, image, amount, number, create_time,dish_flavor) " +
+            "values (#{userId}, #{dishId}, #{setmealId}, #{name}, #{image}, #{amount}, #{number}, #{createTime},#{dishFlavor})")
+    void insert(ShoppingCart shoppingCart);
+}
